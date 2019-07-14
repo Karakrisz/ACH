cf98c750685fd1a5b334cc3efa63d6d63e0a8e94
Krisztian <webevolution13@gmail.com>
2019-07-14 16:55:10 +0200

    fejléc

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <title>ACH</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'
+          integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="//bootstrap-extension.com/js/4.4.0/bootstrap-extension.min.js"></script>
+    <link rel="stylesheet" href="//bootstrap-extension.com/css/4.4.0/bootstrap-extension.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="css/tablet.css" rel="stylesheet" type="text/css"
+          media="only screen and (min-width: 675px) and (max-width: 1200px) "/>
+    <link href="css/mobile.css" rel="stylesheet" type="text/css" media="only screen and (max-width: 674px) "/>
+</head>
+<body>
+
+<div class="offcanvas-main">
+    <div class="offcanvas-container">
+        <div class="navbar navbar-dark bg-dark sticky-top">
+            <a class="navbar-brand kulso_logo" href="#">Logo</a>
+            <button class="offcanvas-toggler" type="button">
+                <i class='fas fa-align-right'></i>
+            </button>
+        </div>
+
+
+        <div class="container-fluid fejlec_tartalom">
+
+            <h5>ALU CUTTING HUNGARY</h5>
+
+            <h1>Alaposság</h1>
+            <h1>Céltudatosság</h1>
+            <h1>Hatékonyság</h1>
+
+            <p>Európa legmodernebb preciziós CNC vágógépei.</p>
+            <p>Az ön sikere a Mi sikerünk!</p>
+
+            <a href="#">BŐVEBBEN </a>
+
+        </div>
+    </div>
+    <div class="offcanvas-menu bg-dark text-light">
+        <div class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="offcanvas-toggler" type="button">
+                <span class="text-light display-7">×</span>
+            </button>
+        </div>
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#menu1">Menu 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#menu2">Menu 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#menu3">Menu 3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#menu4">Menu 4</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+</body>
+</html>
