572ca1bc709ceb0bab5ae3e61511e894ffcb67a0
Krisztian <webevolution13@gmail.com>
2019-07-29 22:45:54 +0200

    labcék rendben

--- index.php
+++ index.php
@@ -289,12 +289,13 @@
 <div class="container col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 lablec">
 
     <div class="row">
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 lablec_elso_tartalom">
 
             <a class="navbar-brand lablec_logo" href="#">
                 <img src="img/ach-logo-v2.png" alt="Logo">
 
             </a>
+
 
 
             <p class="col-sm-5">Lorem ipsum dolor sit amet.</p>
@@ -319,7 +320,7 @@
             </p>
 
 
-            <p>2120 Dunakeszi, Repülőtéri út 1.</p>
+            <p>2120 Dunakeszi,<br>Repülőtéri út 1.</p>
 
             <div class="lablec_menu">
 
